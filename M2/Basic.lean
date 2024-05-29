dff478ecf686a24391c089d559dff34feeb6df24
ymonbru <yannis.monbru@orange.fr>
2024-05-29 16:21:32 +0200

    -

--- M2/Basic.lean
+++ M2/Basic.lean
@@ -1,2 +1,24 @@
+import Mathlib
+
+open CategoryTheory CategoryTheory.Limits TopologicalSpace TopologicalSpace.Compacts Opposite
+
+variable (X) [TopologicalSpace X]
+variable (U:Opens X)
+
+
+#check Compacts X
+
+#check (⊥:Compacts X)
+
+def RelCN (K:Compacts X) : Set (Opens X):= fun (U:Opens X) => (K.carrier ⊆ U.carrier) ∧ IsCompact (closure U.carrier)
+
+#check RelCN X (⊥:Compacts X)
+
+structure Ksheaf where
+  F : (Compacts X)ᵒᵖ ⥤ Ab
+  ksh1 : F.obj (op (⊥:Compacts X)) = zero
+  ksh2 : ∀ K1 K2 :Compacts X, sorry
+  ksh3 : ∀ K:Compacts X, sorry
+
 
 def hello:= "world"
