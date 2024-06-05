40a22cfe6d4d76a4d2fa4790e655bae0d11560f6
ymonbru <yannis.monbru@orange.fr>
2024-06-05 15:47:17 +0200

    -

--- M2/Ksheves.lean
+++ M2/Ksheves.lean
@@ -7,16 +7,10 @@ variable (X) [TopologicalSpace X] [T2Space X]
 variable (U:Opens X) (K:Compacts X)
 variable (F:(Compacts X)ᵒᵖ ⥤ Ab) (K1:Compacts X) (K2:Compacts X)
 
-#check Quiver.Hom.op
-#check Compacts X
-
-#check (⊥:Compacts X)
-
 --La limite
 def RelCN (K:Compacts X) : Set (Opens X) := fun (U:Opens X) => (K.carrier ⊆ U.carrier) ∧ IsCompact (closure U.carrier)
 
 def RelCN_cat (K:Compacts X) : Type := FullSubcategory (RelCN X K)
-#check RelCN X (⊥:Compacts X)
 
 instance : Category (RelCN_cat X K) := FullSubcategory.category (RelCN X K)
 
@@ -31,36 +25,34 @@ lemma closure_increasing (U:Set X) (V:Set X) (h: U ⊆ V): closure U ⊆ closure
 def closureFunc (K:Compacts X): (RelCN_cat X K) ⥤ (Compacts X)  where
   obj U := ⟨closure U.obj.carrier, U.property.2⟩
   map f :=  homOfLE (closure_increasing _ _ _ (leOfHom f))
-  --map_id :=sorry
-  --map_comp:= sorry
 
-def ksh3 : (RelCN_cat X K)ᵒᵖ ⥤ Ab := Functor.comp (closureFunc X K).op  F
+def FUbar : (RelCN_cat X K)ᵒᵖ ⥤ Ab := Functor.comp (closureFunc X K).op  F
 
-#check (Functor.const (RelCN X K)).obj (F.obj (op K)) ⟶ ksh3
+def FK_transNat: (FUbar X K F) ⟶ ((Functor.const (RelCN_cat X K)ᵒᵖ).obj (op K)).comp F where
+app W:= by
+  apply F.map
+  apply op
+  apply homOfLE
+  apply Set.Subset.trans (W.unop).property.1
+  unfold closureFunc closure
+  intro a ha
+  simp [Set.mem_iInter]
+  intro t _ hwt
+  exact hwt ha
+naturality := by
+  intros W Y f
+  unfold FUbar closureFunc
+  simp
+  rw [← F.map_comp]
+  rfl
 
-#check colimit (Functor.comp (closureFunc X K).op  F)
-
-variable (W: RelCN_cat X K)
-
-#check F.obj ((closureFunc X K).op.obj (op W))
-
-
-
-
-
-
-def FK : Cocone (ksh3 X K F):= Cocone.mk (F.obj (op K)) ({app := fun W => (ksh3 X K F).map _  , naturality := sorry} )
-
-#check limit.lift
+def FK : Cocone (FUbar X K F):= Cocone.mk (F.obj (op K)) ((FK_transNat X K F)  ≫ ((Functor.constComp _ _ _).hom))
 
 --Le complexe à 4 terme
 
-#check homOfLE (@bot_le _ _ _ K1)
+open ZeroObject DirectSum
 
-#check (zero:Ab)
-
-#check 0 → F.obj (op (K1 ⊔ K2))
-
+def ZtoFK1uK2:= (0: 0 ⟶ F.obj { unop := K1 ⊔ K2 })
 
 #check F.map (op  (homOfLE (@inf_le_left _ _ K1 K2)))
 
@@ -70,19 +62,18 @@ def FK : Cocone (ksh3 X K F):= Cocone.mk (F.obj (op K)) ({app := fun W => (ksh3 
 
 #check F.map (op  (homOfLE (@le_sup_right _ _ K1 K2)))
 
-
-def complex (F:(Compacts X)ᵒᵖ ⥤ Ab) (K1:Compacts X) (K2:Compacts X): ComposableArrows Ab 3:= CategoryTheory.ComposableArrows.mk₃ sorry sorry sorry
-
+variable (A B:Ab)
 
 
+#check (A ⊕ B)
 
+#check ComposableArrows.mk₃
 
-
-
+--def complex : ComposableArrows Ab 3:= CategoryTheory.ComposableArrows.mk₃ (ZtoFK1uK2 X F K1 K2)  _ _ --sorry
 
 
 structure Ksheaf where
   F : (Compacts X)ᵒᵖ ⥤ Ab
   ksh1 : F.obj (op (⊥:Compacts X)) = zero
   ksh2 : ∀ K1 K2 :Compacts X, sorry
-  ksh3 : ∀ K:Compacts X, sorry
+  ksh3 : ∀ K:Compacts X, (IsIso (colimit.desc (FUbar X K F) (FK X K F)))
