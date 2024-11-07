74e2f2d9a273338706ef37dee660bbfb4b83c46b
ymonbru <yannis.monbru@orange.fr>
2024-11-07 15:05:25 +0100

    -

--- M2/Meta.lean
+++ M2/Meta.lean
@@ -102,13 +102,17 @@ variable (Cat : Type ) [Category Cat]
 variable (A B C D E F G H : Cat) (a : A ⟶ D) (b : A ⟶ C) (c : A ⟶ B) (d : B ⟶ C) (e : C ⟶ E) (f : B ⟶ F) (h : F ⟶ E) (i : E ⟶ G) (j : D ⟶ G) (k : F ⟶ G) (l : G ⟶ H) (m : B ⟶ G) (n : B ⟶ H)
 
 lemma test (h1 : c ≫ d = b) (h2 : b ≫ e = a ≫ g) (h3 : d ≫ e = f ≫ h) (h4 : g ≫ i = j) (h5 : h ≫ i = k) (h6 : f ≫ k = m ) (h7 : m ≫ l = n) : a ≫ j ≫ l = c ≫ n:= by
-  split_square
+  /-split_square
 
-  --rw [← h7, ← h6, ← h5,]
-  --rw_assoc2 h3.map_eq_right
-  --clear h7
-  --clear h6
-  --clear h5
+  rw [← h7, ← h6, ← h5,]
+  rw_assoc2 h3.map_eq_right
+  rw [← h3.map_eq_left]
+  rw_assoc2 h1
+  rw_assoc2 h2.map_eq_left
+  rw[← h2.map_eq_right]
+  rw_assoc h4
+
+  repeat rw [Category.assoc]-/
 
   FindPath
 
