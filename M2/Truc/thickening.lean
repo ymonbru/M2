5e12388c794d901be5eaf301b6ca730448cde42c
ymonbru <yannis.monbru@orange.fr>
2025-10-14 15:29:12 +0200

    --

--- M2/Truc/thickening.lean
+++ M2/Truc/thickening.lean
@@ -2,56 +2,62 @@ import Mathlib
 
 open TopologicalSpace TopologicalSpace.Compacts
 
-variable {X :Type} [TopologicalSpace X] [LocallyCompactSpace X] [T2Space X]
+variable {X}
 
-variable (K1 K2 U : Set X) (h1 : IsCompact K1) (h2: IsCompact K2) (hU : IsOpen U) (h : K1 ∩ K2 ⊆ U)
+--ordered set with a notion of inf
 
-/- The type of small things-/
-def Eps : Type := sorry
+def Eps : Type:= sorry
 
-/- There is a way to compare two elements of Eps-/
 instance : SemilatticeInf Eps := sorry
 
-/- The operation of thickening-/
 def thickening : Set X → Eps → Set X := sorry
 
-lemma selfSubThickening (e : Eps ) (A : Set X) : A ⊆ thickening A e := sorry
+lemma selfSubThickening : ∀ e (A : Set X), A ⊆ thickening A e := sorry
 
 /- A smaller thickening is included in a bigger one-/
-lemma isMonotonusEpsThickening (e f : Eps) (h : e ≤ f) (A : Set X) : thickening A e ⊆ thickening A f := sorry
+lemma isMonotonusEpsThickening : ∀ e f, e ≤ f →  ∀ A : Set X, thickening A e ⊆ thickening A f  := sorry
 
-lemma isMonotonusSetThickening (e : Eps) (A B : Set X) ( h : A ⊆ B): thickening A e ⊆ thickening B e  := by sorry
+lemma isMonotonusSetThickening : ∀ e (A B : Set X), A ⊆ B → thickening A e ⊆ thickening B e := sorry
 
 lemma isMonotonusThickening (e f : Eps) (A B : Set X) (hE : e ≤ f) (hS : A ⊆ B) : thickening A e ⊆ thickening B f := by
   apply Set.Subset.trans
   apply isMonotonusSetThickening _ _ _ hS
   apply isMonotonusEpsThickening _ _ hE
 
-lemma cupMonotonusthinckening (e : Eps) (A B : Set X) : thickening (A ∪ B) e ⊆ thickening A e ∪ thickening B e := sorry
+lemma cupMonotonusthinckening : ∀ e (A B : Set X), thickening (A ∪ B) e ⊆ thickening A e ∪ thickening B e := sorry
 
+
+variable [TopologicalSpace X]
 /- A thickening is always open-/
-lemma isOpenThickening (e : Eps) (A : Set X) : IsOpen <| thickening A e := sorry
+lemma isOpenThickening : ∀ e (A : Set X), IsOpen <| thickening A e := sorry
 
 /- In a T2 space one can draw a line between a compact and a close subset-/
-lemma disjointThickenings (A B : Set X) (hD : Disjoint A B) (h1 : IsCompact A)  (h2 : IsClosed B) :  ∃ e : Eps, Disjoint (thickening A e) (thickening B e) := sorry
+lemma disjointThickenings : ∀ A B : Set X, IsCompact A → IsClosed B → Disjoint A B → ∃ e, Disjoint (thickening A e) (thickening B e) := sorry
 
-lemma compactInOpenThickening (K O : Set X) (hK : IsCompact K) (hO : IsOpen O) (h : K ⊆ O) : ∃ e : Eps, thickening K e ⊆ O := by
+variable [LocallyCompactSpace X] [T2Space X]
+
+lemma compactInOpenThickening : ∀ K O : Set X, IsCompact K → IsOpen O → K ⊆ O →  ∃ e, thickening K e ⊆ O := by
+  intro K O hK hO h
   let F := Oᶜ
-  have hF : IsClosed F := isClosed_compl_iff.2 hO
-  have : Disjoint K F := Set.disjoint_compl_right_iff_subset.2 h
+  have : IsClosed F := by
+    exact isClosed_compl_iff.mpr hO
 
-  let e := (disjointThickenings K F this hK hF).choose
-  let he : Disjoint (thickening K e) (thickening F e) := (disjointThickenings K F this hK hF).choose_spec
+  have : Disjoint K F := by
+    exact Set.disjoint_compl_right_iff_subset.mpr h
 
-  have : Disjoint (thickening K e ) F := Disjoint.mono_right (selfSubThickening e F) he
+  let e := (disjointThickenings K F (by assumption) (by assumption) (by assumption)).choose
+  let he : Disjoint (thickening K e) (thickening F e):= (disjointThickenings K F (by assumption) (by assumption) (by assumption)).choose_spec
 
   use e
-  exact Set.disjoint_compl_right_iff_subset.1 this
+  apply Set.disjoint_compl_right_iff_subset.1
 
+  apply Disjoint.mono_right
+  apply selfSubThickening e
+  assumption
 
+variable (K1 K2 U : Set X) (h1 : IsCompact K1) (h2: IsCompact K2) (hU : IsOpen U) (h : K1 ∩ K2 ⊆ U)
 /- The theorem i want to show-/
-include h1 h2 hU h in
-theorem Goal : ∃ V1 V2 : Set X, IsOpen V1 ∧ IsOpen V2 ∧ K1 ⊆ V1 ∧ K2 ⊆ V2 ∧ V1 ∩ V2 ⊆ U := by
+example : ∃ V1 V2 : Set X, IsOpen V1 ∧ IsOpen V2 ∧ K1 ⊆ V1 ∧ K2 ⊆ V2 ∧ V1 ∩ V2 ⊆ U := by
 
   -- Let V be an open such that K1 ∩ K2 ⊆ V and e such that thickening V e ⊆ U
   -- It's possible in locally comapct spaces.
@@ -101,8 +107,8 @@ theorem Goal : ∃ V1 V2 : Set X, IsOpen V1 ∧ IsOpen V2 ∧ K1 ⊆ V1 ∧ K2 �
     exact K12subV this
 
   -- let e2 be sucht thath the thickenings of A and B of length e2 are disjoint.
-  let e2 : Eps := (disjointThickenings A B hAB hA hB).choose
-  let he : Disjoint (thickening A e2) (thickening B e2):= (disjointThickenings A B hAB hA hB).choose_spec
+  let e2 : Eps := (disjointThickenings A B hA hB hAB).choose
+  let he : Disjoint (thickening A e2) (thickening B e2):= (disjointThickenings A B hA hB hAB).choose_spec
 
   -- let e be the infimum of e1 and e2 wich will be the length of thickening to obtain V1 and V2
   let e := e1 ⊓ e2
