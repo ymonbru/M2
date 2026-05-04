26817bdd473494f58c38d22c80c76c9f049f0fb6
ymonbru <yannis.monbru@orange.fr>
2026-05-04 15:07:51 +0200

    --

--- M2/Propre/unionCat.lean
+++ M2/Propre/unionCat.lean
@@ -1,18 +1,169 @@
 import M2.Propre.Topology
 import M2.forceColimW
+import Mathlib
 
-open CategoryTheory Limits TopologicalSpace Compacts Opposite Functor
+open CategoryTheory Limits TopologicalSpace Compacts Opposite Functor Pseudofunctor.StrongTrans
 
 universe u1 u2 u3 u4 v1 v2 v3 v4
+namespace CategoryTheory.Bicategory
+variable {B C : Type*} [Bicategory B] [Bicategory C]
+variable {F : B ⥤ᵖ C}
+
+@[simps]
+def Pseudofunctor.constObj {J C : Type*} [Bicategory J] [Bicategory C] (x : C) : J ⥤ᵖ C where
+  obj _ := x
+  map _ := 𝟙 _
+  map₂ _ := 𝟙 _
+  mapId _ := eqToIso rfl
+  mapComp _ _ := (λ_ (𝟙 x)).symm
+
+
+set_option backward.isDefEq.respectTransparency false in
+@[simps]
+def Pseudofunctor.constMap {J C : Type*} [Bicategory J] [Bicategory C] {x y : C } (f : x ⟶ y) : Pseudofunctor.constObj (J := J) x ⟶ Pseudofunctor.constObj y  where
+app _ := f
+naturality j := bicategoricalIso (𝟙 x ≫ f) (f ≫ 𝟙 y)
+
+set_option backward.isDefEq.respectTransparency false in
+@[simps]
+def PseudoFunctor.constMap₂ {J C : Type*} [Bicategory J] [Bicategory C] {x y : C } {f g: x ⟶ y} (τ : f ⟶ g) : Pseudofunctor.constMap (J := J) f ⟶ Pseudofunctor.constMap g  where
+  as := ⟨ fun _ => τ, fun _ => by simp [Pseudofunctor.constMap]⟩
+
+set_option backward.isDefEq.respectTransparency false in
+@[simps]
+def Pseudofunctor.const {C : Type*} (J : Type*) [Bicategory J] [Bicategory C] : C ⥤ᵖ J ⥤ᵖ C where
+  obj := Pseudofunctor.constObj
+  map := Pseudofunctor.constMap
+  map₂ := PseudoFunctor.constMap₂
+  mapId _ := by
+    refine Pseudofunctor.StrongTrans.isoMk ?_ ?_
+    · intro _
+      rfl
+    · intro _ _ _
+      simp
+  mapComp _ _ := by
+    refine Pseudofunctor.StrongTrans.isoMk ?_ ?_
+    · intro _
+      rfl
+    · intro _ _ _
+      simp
+
+#check Pseudofunctor.const
+
+variable (F) in
+structure Cocone where
+  pt : C
+  ι : Pseudofunctor.StrongTrans F ((Pseudofunctor.const B).obj pt)
+
+--def Cocone.w (s : Cocone F) {x y : B} (f : x ⟶ y) : F.map f ≫ s.ι.app x ≅ s.ι.app y := by sorry
+@[simps]
+def Cocone.extend (c : Cocone F) {x : C} (f : c.pt ⟶ x ) : Cocone F where
+  pt := x
+  ι := c.ι ≫ (Pseudofunctor.const B).map f
+
+def Cocone.whisker {D : Type*} [Bicategory D] (E : D ⥤ᵖ B) (c : Cocone F) : Cocone (E.comp F) where
+  pt := c.pt
+  ι := by
+
+    --est-ce que j'ai vraiment besoin de ça?
+    sorry
+
+
+
+variable {B : Type*} [Category B]
+variable {I : LocallyDiscrete B ⥤ᵖ Cat} (c : Cocone I)
+
+set_option backward.isDefEq.respectTransparency false in
+@[simps]
+def truc : I.Grothendieck ⥤ c.pt where
+  obj g := (c.ι.app ⟨g.1⟩).toFunctor.obj g.2
+  map {g h} f := (c.ι.naturality f.1.toLoc).inv.toNatTrans.app g.2 ≫ (c.ι.app ⟨h.1⟩).toFunctor.map f.2
+  map_id x := by
+    suffices (c.ι.app ⟨x.base⟩).toFunctor.map _ = (c.ι.naturality _ ).hom.toNatTrans.app x.fiber  by
+      rw [this]
+      simp
+    have : (c.ι.naturality (𝟙 x : x ⟶ x).base.toLoc ).hom = ?_ := by simpa using c.ι.naturality_id ⟨x.1⟩
+
+    rw [this]
+    simp
+  map_comp {x y z } f g:= by
+
+    suffices (c.ι.app { as := z.base }).toFunctor.map (f ≫ g).fiber = (c.ι.naturality (f ≫ g).base.toLoc).hom.toNatTrans.app x.fiber ≫ ?_ by
+      rw [this, ← Category.assoc]
+      simp
+      rfl
+
+    let h := c.ι.naturality_comp f.1.toLoc g.1.toLoc
+    --let h := NatTrans.ext_iff.1 (Cat.Hom₂.ext_iff.1 (c.ι.naturality_comp f.1.toLoc g.1.toLoc))
+
+    simp at h
+
+    have : (c.ι.naturality (f.base.toLoc ≫ g.base.toLoc)).hom = ?_ ≫ (α_ (c.ι.app { as := x.base }) (𝟙 c.pt) (𝟙 c.pt)).inv≫ (ρ_ (c.ι.app { as := x.base } ≫ 𝟙 c.pt)).hom := by
+      --simpa using h
+      sorry
+
+    --rw [← h]
+    sorry
+    sorry
+
+instance : IsCofilteredOrEmpty I.Grothendieck where
+  cone_objs d1 d2 := by
+
+
+
+
+    sorry
+  cone_maps d1 d2 f1 f2 := by
+
+
+    sorry
+
+instance : (truc c).Initial := by
+  rw [Functor.initial_iff_of_isCofiltered]
+  constructor
+  · intro d
+    simp
+    sorry
+  · intro d ⟨a,x⟩ f1 f2
+    simp at f1
+    simp at f2
+    simp
+
+    sorry
+
+variable {D : Cat}
+variable (F : c.pt ⟶ D)
+
+#check Bicategory.Cocone.extend c
+
+set_option backward.isDefEq.respectTransparency false in
+lemma hey : truc (c.extend F) = truc c ⋙ F.toFunctor := Functor.ext (by simp) (by simp)
+
+
+
+
+end CategoryTheory.Bicategory
 
 namespace CategoryTheory.Limits.UnionCat
-variable {A : Type u1} [Category.{v1, u1} A] {I : A ⥤ Cat.{v2, u2}} {D : Type u3} [Category.{v3, u3} D]
+variable {A : Type u1} [Category.{v1, u1} A] {D : Type u3} [Category.{v3, u3} D]
+variable {I : A ⥤ Cat.{v2, u2}}
 
-variable (D) in
+#check I.toPseudofunctor'
+
+#check Pseudofunctor.Grothendieck.forget
+
+--#check Pseudofunctor.StrongTrans I.toPseudofunctor' ((Functor.const A).obj self.pt).toPseudofunctor'
+
+variable (I) in
+structure test  where
+  pt : Cat
+  ι : Pseudofunctor.StrongTrans I.toPseudofunctor' ((Functor.const A ).obj pt).toPseudofunctor'
+
+variable (D I) in
 /-- The data of a Cocone for F, but with isomorphism instead of equality and the lemmas that allow computation
 
 D is not part of the structure to avoid issue in inferance later-/
-structure CoconeFunctor (I : A ⥤ Cat.{v2, u2}) where
+structure CoconeFunctor where
   /-- the canonial morphisms of the cocone-/
   i : (x : A) → (I.obj x) ⥤ D
   /-- The w condition of the cocone as an isomorphism-/
@@ -21,6 +172,35 @@ structure CoconeFunctor (I : A ⥤ Cat.{v2, u2}) where
   isoId : (x  : A) → (iso (𝟙 x)) = eqToIso (by simp [Functor.id_comp])
   /-- The compatibility condition over iso(f ≫ g): it equals the isomorphism induced by equality F.map (f ≫ g) = F.map f ≫ F.map g )-/
   isoComp : {x y z: A } → (f : x ⟶ y) → (g : y ⟶ z) → iso (f ≫ g) = isoWhiskerRight (eqToIso ((Cat.ext_iff.mp (I.map_comp _ _)).trans (Cat.Hom.comp_toFunctor _ _))) (i z) ≪≫ associator (I.map f).toFunctor (I.map g).toFunctor (i z) ≪≫ isoWhiskerLeft (I.map f).toFunctor (iso g) ≪≫ iso f
+
+set_option backward.isDefEq.respectTransparency false in
+def truc (t :test I) : CoconeFunctor t.pt I where
+  i x := (t.ι.app ⟨x⟩).toFunctor
+  iso f := by
+    let h := t.ι.naturality ⟨f⟩
+
+    simp only [toPseudofunctor'_obj, const_obj_obj, toPseudofunctor'_map, const_obj_map,
+      Category.comp_id] at h
+
+
+    let h := t.ι.naturality_naturality (eqToHom rfl :⟨f⟩ ⟶ ⟨f⟩ )
+    simp at h
+    --ça c'est trivial du coup
+    simp
+    sorry
+  isoId x := by
+    let h := t.ι.naturality_id ⟨x⟩
+    simp at h
+
+
+
+    sorry
+  isoComp f g:= by
+    let h := t.ι.naturality_comp ⟨f⟩ ⟨g⟩
+    simp at h
+
+
+    sorry
 
 variable {C : Type u4} [Category.{v4, u4} C] (sD : CoconeFunctor D I)
 
